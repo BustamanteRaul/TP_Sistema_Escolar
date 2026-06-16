428d270471b7fbd48f1737c65748a8aec5e26ea1
m4ru4v4l0s <Marvalen1124@gmail.com>
2026-06-16 11:25:50 -0300

    clases

---
+++ src/Profesor.js
@@ -0,0 +1,34 @@
+const Persona = requirer ("./Persona");
+
+class Profesor extends Persona {
+    constructor(nombre, dni, email) {
+        super(nombre, dni, email);
+
+        this.cursos = [];
+    }
+
+    asignarCurso(curso) {
+        if (!this.cursos.includes(curso)) {
+            this.cursos.push(curso);
+        }
+
+        curso.agregarProfesor(this);
+    }
+
+    registrarNota(curso, alumno, nota) {
+        curso.cargarNota(alumno, nota);
+    }
+
+    registrarFalta(curso, alumno) {
+        curso.cargarFalta(alumno);
+    }
+
+    reporte() {
+        console.log( "Reporte del profesor ${this.nombre}");
+        this.cursos.forEach((curso) => {
+            console.log("Dicta: ${curso.nombre} (codigo: ${curso.codigo})");
+        });
+    }
+}
+
+module.exports = Profesor;
